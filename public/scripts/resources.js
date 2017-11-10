bb1797262e781fa2f27ade29d0e433c194402c4f
Stéfano Stypulkowski Zanata <made.in.stefano@gmail.com>
2017-11-10 19:34:11 -0200

    Added more refactors

---
+++ public/scripts/resources.js
@@ -0,0 +1,6 @@
+window.__resources = {
+  audio: {
+    click: "https://s3-sa-east-1.amazonaws.com/szanata/minefield/fx/square_click.wav",
+    explosion: "https://s3-sa-east-1.amazonaws.com/szanata/minefield/fx/explosion.wav"
+  }
+};
